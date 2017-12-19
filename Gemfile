4a8dca042003d60966031243ab034b6d92365f03
Michel Julien <michel_julien@videotron.ca>
2017-12-18 19:22:13 -0500

    Demander d'aller au system fonctionne bien

--- Gemfile
+++ Gemfile
@@ -5,6 +5,9 @@ git_source(:github) do |repo_name|
   "https://github.com/#{repo_name}.git"
 end
 
+# BOOTSTRAP: Michel julien
+gem 'bootstrap-sass', '~> 3.3.7'
+#gem 'sass-rails', '>= 3.2'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.1.4'
---
+++ valider.sh
@@ -0,0 +1,7 @@
+cd
+cd hor13
+#~ metaClasses="EPS1A EPS1B EPS2A EPS2B EPS1C_29 EPS3B EPS4C_39 EPS4B_5B EPS5C_45 EPS5C_46"
+#~ metaClasses="EPS1A EPS1B EPS2A EPS2B EPS1C_29 EPS3B EPS4C_39 EPS4B_5B EPS5C_45 EPS5C_46"
+cat op/cedulables/nil.txt | ruby mapred/repartir.rb $1  | mapred/valider_01
+cd
+cd rails_projects/hor
